59fa4bce7590436661b4d08f10f5a2c5aed972ef
Alex-com <azurart@gamil.com>
2025-10-28 09:32:16 +0200

    28.10

---
+++ test_1.py
@@ -0,0 +1,24 @@
+from selene import browser, have
+
+import pytest
+
+
+@pytest.fixture
+def test_success_login():
+    browser.open_url('https://niffler.qa.guru')
+    browser.element('[id="username"]').type('stas')
+    browser.element('[id="password"]').type('12345')
+    browser.element('[id="login-button"]').click()
+
+    browser.element('[id="spendings"]').should(have.text('History of Spendings'))
+    #browser.quit()
+
+@pytest.fixture
+def test_success_login_with_press_enter():
+    browser.open_url('https://niffler.qa.guru')
+    browser.element('[id="username"]').type('stas')
+    browser.element('[id="password"]').type('12345').press_enter()
+
+    browser.element('[id="spendings"]').should(have.text('History of Spendings'))
+    #browser.quit()
+
--- test_2.py
+++ test_2.py
@@ -1,5 +1,3 @@
-from ftplib import print_line
-
 import pytest
 
 
